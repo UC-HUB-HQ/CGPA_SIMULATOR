e3b1230c34c879cbc48014bba17bc6c90bfd513a
okondev2021 <sojapril16@gmail.com>
2024-04-03 21:56:02 +0100

    improved input validation

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,7 +1,7 @@
 const Info = () => {
     return(
        <div>
-         <p className="mt-4 text-base text-center">Found this helpful? <a className="font-semibold underline text-primary-100" href="https://uchubwaitlist.netlify.app/">See more from UC Hub.</a></p>
+         <p className="mt-4 text-base text-center mobile:text-sm">Found this helpful? <a className="font-semibold underline text-primary-100" href="https://uchubwaitlist.netlify.app/">See more from UC Hub.</a></p>
        </div>
     )
 }
