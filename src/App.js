edcb6a5838008aa03b96694fd2fa9e77548ccd5c
okondev2021 <sojapril16@gmail.com>
2024-05-06 20:12:42 -0700

    added new share to twitter feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import { Hero, Nav, Calculator,Info, Footer } from './components';
 function App() {
+  const [score, setScore] = useState(0);
+  
+  const getCGPA = (calculatedCGPA) => {
+    setScore(calculatedCGPA)
+  }
   return (
     <div className='min-h-screen px-24 bg-black-bodyBgColor tab:bg-black-calcBgColor md:px-8 font-body1 mobile:px-0'>
       <Nav />
       <Hero />
-      <Calculator />
-      <Info />
-        <Footer />
+      <Calculator handleScore={getCGPA} />
+      <Info cgpa={score} />
+      <Footer />
     </div>
   );
 }
--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,9 +1,24 @@
-const Info = () => {
-    return(
-       <div>
-         <p className="mt-4 text-base text-center mobile:text-sm">Found this helpful? <a className="font-semibold underline text-primary-100" href="https://uchubwaitlist.netlify.app/">See more from UC Hub.</a></p>
-       </div>
-    )
+const Info = ({cgpa}) => {
+  
+  const message = `I tried the UC Hub CGPA calculator, here's my score: ${cgpa}. You should try it out too.`
+  // const twitterBtn = useRef(null)
+  // twitterBtn.href = `http://twitter.com/share?&text=${message}&hashtags=uchub,cgpacalculator`
+  
+  return(
+    <section>
+      <div>
+        <p className="mt-4 text-base text-center mobile:text-sm">Found this helpful? <a className="font-semibold underline text-primary-100" href="https://uchubwaitlist.netlify.app/">See more from UC Hub.</a></p>
+      </div>
+      <div className="flex flex-col items-center justify-center mt-10 ">
+        <h3 className="text-center mobile:w-[70%]">Satisfied with our CGPA generator? Share your score with us on twiiter?</h3>
+        <a href={`http://twitter.com/share?&url=&text=${message}&hashtags=uchub,cgpacalculator`} className="flex items-center justify-center gap-2 px-5 py-3 mt-6 rounded-full bg-primary-700 mobile:mt-4">
+          <i class="bi bi-twitter-x"></i>
+          <span className="text-[12px] font-bold ">Continue with Twitter</span>
+        </a>
+      </div> 
+    </section>
+  )
 }
 
 export default Info
+
