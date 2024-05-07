2519559859b70afe261f3bb2ddddb593a6e91db3
okondev2021 <sojapril16@gmail.com>
2024-05-07 06:10:24 -0700

    fixing url issue: testing message

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,6 +1,6 @@
 const Info = ({cgpa}) => {
   
-  // const message = encodeURIComponent(`I tried the UC Hub CGPA calculator, here's my score: ${cgpa}. You should try it out too.`)
+  const message = encodeURIComponent(`I tried the UC Hub CGPA calculator, here's my score: ${cgpa}. You should try it out too.`)
   // const link = encodeURIComponent("https://uchubwaitlist.netlify.app/")
   // const twitterBtn = useRef(null)
   // twitterBtn.href = `http://twitter.com/share?&text=${message}&hashtags=uchub,cgpacalculator`
@@ -14,7 +14,7 @@ const Info = ({cgpa}) => {
       </div>
       <div className="flex flex-col items-center justify-center mt-10 ">
         <h3 className="text-center mobile:w-[70%]">Satisfied with our CGPA generator? Share your score with us on twiiter?</h3>
-        <a href="https://twitter.com/intent/tweet" class="twitter-share-button flex items-center justify-center gap-2 px-5 py-3 mt-6 rounded-full bg-primary-700 mobile:mt-4" data-size="large" data-text={`I tried the UC Hub CGPA calculator, here's my score: ${cgpa}. You should try it out too`} data-via="@uc_hub_" data-hashtags="uchub cgpacalculator" data-url=" "  data-show-count="false">
+        <a rel="canonical" href={`https://twitter.com/intent/tweet?text=${message}`} class="twitter-share-button flex items-center justify-center gap-2 px-5 py-3 mt-6 rounded-full bg-primary-700 mobile:mt-4" data-size="large" data-text={`I tried the UC Hub CGPA calculator, here's my score: ${cgpa}. You should try it out too`} data-via="@uc_hub_" data-hashtags="uchub cgpacalculator" data-url=" "  data-show-count="false">
           <i class="bi bi-twitter-x"></i>
           <span className="text-[12px] font-bold ">Continue with Twitter</span>
         </a>
