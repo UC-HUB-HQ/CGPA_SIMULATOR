a6584d6ec5dffdc8b9501ecc94483f318ec92d2d
okondev2021 <sojapril16@gmail.com>
2024-05-08 17:31:35 -0700

    updated the text

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,12 +1,5 @@
-const Info = ({cgpa}) => {
-  
-  const message = encodeURIComponent(`I tried the UC Hub CGPA calculator, here's my score: ${cgpa}. You should try it out too.`)
-  // const link = encodeURIComponent("https://uchubwaitlist.netlify.app/")
-  // const twitterBtn = useRef(null)
-  // twitterBtn.href = `http://twitter.com/share?&text=${message}&hashtags=uchub,cgpacalculator`
-  // http://x.com/share?&url=${link}&text=${message}&hashtags=uchub,cgpacalculator
-  
-  
+const Info = ({cgpa}) => {  
+  const message = encodeURIComponent(`I tried the UC Hub CGPA calculator, here's my score: ${cgpa}. You should try it out too.`)  
   return(
     <section>
       <div>
@@ -14,7 +7,7 @@ const Info = ({cgpa}) => {
       </div>
       <div className="flex flex-col items-center justify-center mt-10 ">
         <h3 className="text-center mobile:w-[70%]">Satisfied with our CGPA generator? Share your score with us on twiiter?</h3>
-        <a rel="canonical" href={`https://twitter.com/intent/tweet?text=${message}&hashtags=uchub,cgpacalculator&via=uc_hub_`} class="twitter-share-button flex items-center justify-center gap-2 px-5 py-3 mt-6 rounded-full bg-primary-700 mobile:mt-4" data-size="large" data-show-count="false">
+        <a rel="canonical" href={`https://twitter.com/intent/tweet?text=${message}&hashtags=uchub,cgpasimulator, cgpashowoff,cgpagoals&via=uc_hub_&url=https://uchubwaitlist.netlify.app/`} class="twitter-share-button flex items-center justify-center gap-2 px-5 py-3 mt-6 rounded-full bg-primary-700 mobile:mt-4" data-size="large" data-show-count="false">
           <i class="bi bi-twitter-x"></i>
           <span className="text-[12px] font-bold ">Continue with Twitter</span>
         </a>
