5d99649bf401b2a98126fdab47494f7cd1e62c3c
Okon Jedidiah Solomon <96173434+okondev2021@users.noreply.github.com>
2025-01-07 01:50:07 +0100

    Update footer.jsx
    Update copyright year

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return(
        <div className="relative w-full h-[25vh] flex justify-center items-center text-sm pb-4">
-         <p className="absolute bottom-0 block pb-4 text-center ">&copy; 2024 U.C Hub, All rights reserved</p>
+         <p className="absolute bottom-0 block pb-4 text-center ">&copy; 2025 U.C HUB, All rights reserved</p>
        </div>
     )
 }
